3dce3526d66f542fc8f54a0e48a3b44984d6d05d
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-14 18:44:57 +0530

    finding position

---
+++ 14-Mar-2022/src/String2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.*;
+public class String2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println("Original string:"+s);
+        System.out.println("position of o is:"+s.indexOf("o"));
+    }
+}
