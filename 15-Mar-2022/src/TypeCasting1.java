cd93caf758fca5ed72c7d8f524f326ec9612f8b6
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-15 12:47:53 +0530

    automatic conversion

---
+++ 15-Mar-2022/src/TypeCasting1.java
@@ -0,0 +1,12 @@
+public class TypeCasting1 {
+    public static void main(String[]args){
+        int i = 10;
+        long l = i;
+        float f=l;
+        double d=i;
+        System.out.println("integer number is:"+i);
+        System.out.println("long number is:"+l);
+        System.out.println("float number is:"+f);
+        System.out.println("double number is:"+d);
+    }
+}
