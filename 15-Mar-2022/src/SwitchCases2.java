923d2bd36dd25ba7800e4922e17abe4cebdb4b49
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-15 13:44:12 +0530

    Switch cases in wrapper class

---
+++ 15-Mar-2022/src/SwitchCases2.java
@@ -0,0 +1,27 @@
+public class SwitchCases2 {
+    public static void main(String[]args){
+        Integer i = 5;
+        switch(i){
+            case 1:
+            System.out.println("the value is "+i);
+            break;
+            case 2:
+                System.out.println("the value is "+i);
+            break;
+            case 3:
+                System.out.println("the value is "+i);
+            break;
+            case 4:
+                System.out.println("the value is "+i);
+            break;
+            case 5:
+                System.out.println("the value is "+i);
+            break;
+            default:
+                System.out.println("enter the value ");
+            break;
+
+
+        }
+    }
+}
