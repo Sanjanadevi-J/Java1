873d4fd9ae3f992f2242a24a15dc14c77cc6c7a1
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-15 10:52:40 +0530

    unboxing

---
+++ 15-Mar-2022/src/WrapperClass2.java
@@ -0,0 +1,9 @@
+public class WrapperClass2 {
+    public static void main(String[]args){
+        Integer i = new Integer(30);
+        int n =i.intValue();
+        System.out.println("Number is:"+n);
+        System.out.println("Wrapper class object is:"+i);
+
+    }
+}
