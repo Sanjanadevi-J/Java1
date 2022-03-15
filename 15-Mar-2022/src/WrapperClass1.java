7027f7238bcd8dfe910769561bb5a63f060fc9db
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-15 10:52:04 +0530

    autoboxing

---
+++ 15-Mar-2022/src/WrapperClass1.java
@@ -0,0 +1,8 @@
+public class WrapperClass1 {
+    public static void main(String[]args){
+        int number = 100;
+        Integer i=Integer.valueOf(number);
+        System.out.println(i);
+        System.out.println(number);
+    }
+}
