9f83f5c900699ddd16171d2457cf55090f467718
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-21 11:11:15 +0530

    Map interface methods

---
+++ 21-Mar-2022/src/Map1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+import static java.util.Map.*;
+
+public class Map1 {
+    public static void main(String[]args){
+        Map<Integer,String> names = new HashMap<Integer,String>();
+        names.put(500,"Amir");
+        names.put(501,"Krish");
+        names.put(502,"Ronald");
+        names.put(503,"Amir");
+        System.out.println(names);
+        for(Entry name: names.entrySet()){
+            System.out.println(name.getKey()+" "+name.getValue());
+        }
+        System.out.println("Size of map is:"+names.size());
+        System.out.println("names contains 300?:"+names.containsKey(300));
+        System.out.println("names contains Amir?:"+names.containsValue("Amir"));
+        System.out.println("Entry set is:"+names.entrySet());
+
+    }
+}
